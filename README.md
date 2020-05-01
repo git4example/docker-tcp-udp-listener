9f5f92c4280a0c4457aa2a24f30f21e4da687223
hello2parikshit <parixitpatel@gmail.com>
2020-05-01 14:02:52 +1000

    few changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # docker-tcp-udp-listener
+
+
+Run client to test TCP connection
+
+python tcpclient.py <serverip/hostname> <port>
--- tcpudplistener.py
+++ tcpudplistener.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import sys
 
 
 UDP_IP = "0.0.0.0"
@@ -7,10 +8,40 @@ UDP_PORT = int(os.environ['UDPPORT'])
 
 print "Listening on UDP port",UDP_PORT
 
-sock = socket.socket(socket.AF_INET, # Internet
+udpsock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+udpsock.bind((UDP_IP, UDP_PORT))
+
+
+
+TCP_IP = "0.0.0.0"
+TCP_PORT = int(os.environ['TCPPORT'])
+
+print "Listening on TCP port",TCP_PORT
+
+tcpsock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_STREAM) # TCP
+tcpsock.bind((TCP_IP, TCP_PORT))
+
+# Listen for incoming connections
+tcpsock.listen(1)
+
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print data
+    udpdata, addr = udpsock.recvfrom(1024) # buffer size is 1024 bytes
+    print udpdata
+
+    # Wait for a TCP connection
+     print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = tcpsock.accept()
+    try:
+        print >>sys.stderr, 'client connected:', client_address
+        while True:
+            tcpdata = connection.recv(16)
+            print >>sys.stderr, 'received "%s"' % tcpdata
+            if tcpdata:
+                connection.sendall(tcpdata)
+            else:
+                break
+    finally:
+        connection.close()
